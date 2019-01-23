4cfb2c9b22dcd840ceed85650e26b1d6e79ce66d
Cody Rush <codyrush@gmail.com>
2019-01-23 12:37:16 -0800

    updated api call in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
 var projectTitle = "Project One";
+
+
+var queryURL = "http://api.eventful.com/json/events/search?";
+var appKey = "ZsBNvKB3vPZxBcL9";
+
+$.ajax({
+    
+    url: queryURL + "app_key=" + appKey + "&q=San+Diego",
+    method:"GET"
+
+}).then(function(response) {
+
+    console.log(response);
+
+})
