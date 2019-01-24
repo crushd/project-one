c8f50f8d633f8a36a0f6b53d4bfe648a21a210a2
cwolberg <cwolberg22@gmail.com>
2019-01-23 17:22:47 -0800

    Add files via upload

---
+++ pseudo.js
@@ -0,0 +1,9 @@
+/*
+jquery populates location/category page
+take user input of location and event category/ies
+store them as an array if you have an account or store as variable
+
+category chosen and location will communicate with api's
+jquery populates event page
+events will be listed in a table, pulling from api to show details
+*/
