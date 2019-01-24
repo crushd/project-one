2613d500b72baa02356c0964c3ae6099cacaf840
codyrush <codyrush@gmail.com>
2019-01-23 18:53:52 -0800

    added a getEventsByLocationId function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 var projectTitle = "Project One";
 var songkickApiKey = "XFK6hX8iZ4LjPg6l";
-
+var thisLocationId;
 
 
 
@@ -23,9 +23,9 @@ function eventfulCall() {
 }
 
 function getLocationId(query) {
-
+    //var getRidofSpacesInQuery = query.split(" ").join("") 
     var queryUrl = "https://api.songkick.com/api/3.0/search/locations.json?query=" + query + "&apikey=" + songkickApiKey;
-    
+    console.log("queryURL",queryUrl)
     $.ajax({
             
         url: queryUrl,
@@ -37,33 +37,44 @@ function getLocationId(query) {
 
         var locationArray = response.resultsPage.results.location;
 
-        for (i=0; i<locationArray.length;i++) {
+        for (i=0;i<locationArray.length;i++) {
+            if (locationArray[i].metroArea.country.displayName === "US") {
 
-            var locationMetro = locationArray[i].metroArea.displayName;
-            var locationMetroState = locationArray[i].metroArea.state.displayName;
+                var locationMetro = locationArray[i].metroArea.displayName;
+                var locationMetroState = locationArray[i].metroArea.state.displayName;
+                console.log(locationArray[i].metroArea.id + ": " + locationMetro + ", " + locationMetroState);
+                
 
-            console.log(locationArray[i].metroArea.id + ": " + locationMetro + ", " + locationMetroState);
+            }
+
         }
 
-    })
+    }).catch(
+        function(error){
+            console.log(error)
+        }
+    )
 
 }
 
-function songkickCall() {
+function getEventsByLocationId(thisLocationId) {
 
-    var metroAreaId = "San+Diego+CA";
-    var queryURL = "https://api.songkick.com/api/3.0/metro_areas/" + metroAreaId + "/calendar.json?apikey=" + songkickApiKey;
     
-
-        $.ajax({
+    var queryUrl = "https://api.songkick.com/api/3.0/metro_areas/"+ thisLocationId + "/calendar.json?apikey=" + songkickApiKey;
+    
+    $.ajax({
             
-            url: queryURL + "app_key=" + appKey + "&q=San+Diego",
-            method:"GET"
+        url: queryUrl,
+        method:"GET"
 
-        }).then(function(response) {
+    }).then(function(response) {
 
-            console.log(response);
+        console.log(response.resultsPage.results.event);
 
-        })
-    
+    }).catch(
+        function(error){
+            console.log(error)
+        }
+    )
+
 }
