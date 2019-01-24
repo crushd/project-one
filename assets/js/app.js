0a134bc402dc5ce04b078c552e63e8593f9916e8
codyrush <codyrush@gmail.com>
2019-01-23 17:58:18 -0800

    added getLocationId function to query songkick for a metro area id

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,69 @@
 var projectTitle = "Project One";
+var songkickApiKey = "XFK6hX8iZ4LjPg6l";
 
 
-var queryURL = "http://api.eventful.com/json/events/search?";
-var appKey = "ZsBNvKB3vPZxBcL9";
 
-$.ajax({
+
+function eventfulCall() {
+
+    var queryURL = "http://api.eventful.com/json/events/search?";
+    var appKey = "ZsBNvKB3vPZxBcL9";
+
+        $.ajax({
+            
+            url: queryURL + "app_key=" + appKey + "&q=San+Diego",
+            method:"GET"
+
+        }).then(function(response) {
+
+            console.log(response);
+
+        })
     
-    url: queryURL + "app_key=" + appKey + "&q=San+Diego",
-    method:"GET"
+}
 
-}).then(function(response) {
+function getLocationId(query) {
 
-    console.log(response);
+    var queryUrl = "https://api.songkick.com/api/3.0/search/locations.json?query=" + query + "&apikey=" + songkickApiKey;
+    
+    $.ajax({
+            
+        url: queryUrl,
+        method:"GET"
 
-})
+    }).then(function(response) {
+
+        //console.log(response.resultsPage.results.location);
+
+        var locationArray = response.resultsPage.results.location;
+
+        for (i=0; i<locationArray.length;i++) {
+
+            var locationMetro = locationArray[i].metroArea.displayName;
+            var locationMetroState = locationArray[i].metroArea.state.displayName;
+
+            console.log(locationArray[i].metroArea.id + ": " + locationMetro + ", " + locationMetroState);
+        }
+
+    })
+
+}
+
+function songkickCall() {
+
+    var metroAreaId = "San+Diego+CA";
+    var queryURL = "https://api.songkick.com/api/3.0/metro_areas/" + metroAreaId + "/calendar.json?apikey=" + songkickApiKey;
+    
+
+        $.ajax({
+            
+            url: queryURL + "app_key=" + appKey + "&q=San+Diego",
+            method:"GET"
+
+        }).then(function(response) {
+
+            console.log(response);
+
+        })
+    
+}
