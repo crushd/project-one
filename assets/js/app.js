2bb846d82fa6b4dcc82ba7a4dcf050b910b407c5
codyrush <codyrush@gmail.com>
2019-01-28 19:23:28 -0800

    added links to artist, venue, metro, and link to the event details.

--- assets/js/app.js
+++ assets/js/app.js
@@ -58,10 +58,21 @@ function getEventsByLocationId(thisLocationId) {
 
         var eventArray = response.resultsPage.results.event;
 
+        // container = $('#documents');
+
         for (e=0; e< eventArray.length; e++) {
             console.log(eventArray[e].displayName);
+            var artistURI = eventArray[e].performance[0].artist.uri;
+            var eventURI = eventArray[e].uri;
+            var venueURI = eventArray[e].venue.uri;
+            var metroURI = eventArray[e].venue.metroArea.uri;
+            //console.log(eventArray[e].uri);
 
-            $("#full-event-list").append("<tr class='well'><td>"+ moment(eventArray[e].start.date).format("MMM Do YYYY")  +"</td><td class='event-name'>" + eventArray[e].performance[0].displayName + "</td><td>" + eventArray[e].venue.displayName +"</td><td>"+ eventArray[e].location.city +"</td></tr>");
+            $("#full-event-list").append("<tr class='well'><td class='event-date'>"+ moment(eventArray[e].start.date).format("MMM Do YYYY")  +"</td><td>" + "<a class='event-name' href='"+artistURI+"' target='_blank'>" + eventArray[e].performance[0].displayName + "</a>" + "</td><td>" + "<a class='event-venue' href='"+venueURI+"' target='_blank'>" + eventArray[e].venue.displayName +"</a></td><td>" + "<a class='event-metro' href='"+metroURI+"' target='_blank'>" + eventArray[e].location.city +"</a></td><td><a href='"+eventURI+"' class='btn btn-primary event-details' target='_blank'>Details</a></td></tr>");
+
+            /* var item = $('<div>').addClass('card'),
+                //title = $('<div>').addClass('card-title'),
+                body = $('<div>').addClass('card-body'); */
 
         }
 
